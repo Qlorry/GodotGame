7f6b37255d30fe2613ac8fd4d8ffd772d898e711
Kostiantyn Kucher <kostik.cu4er@gmail.com>
2024-10-05 17:48:58 +0300

    tyrn based system

---
+++ scripts/player.gd
@@ -0,0 +1,64 @@
+#extends CharacterBody2D
+#
+#
+#const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0
+#
+#
+#func _physics_process(delta: float) -> void:
+	## Add the gravity.
+	##if not is_on_floor():
+		##velocity += get_gravity() * delta
+#
+	## Handle jump.
+	##if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		##velocity.y = JUMP_VELOCITY
+#
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction := Input.get_axis("ui_left", "ui_right")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+	#move_and_slide()
+
+
+extends CharacterBody2D
+
+var animation_speed = 2
+var moving = false
+var tile_size = 64
+var inputs = {
+	"right": Vector2.RIGHT,
+	"left": Vector2.LEFT,
+	"up": Vector2.UP,
+	"down": Vector2.DOWN
+}
+@onready var ray: RayCast2D = $RayCast2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready():
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size / 2
+	
+func _unhandled_input(event):
+	if moving:
+		return
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			move(dir)
+			
+func move(dir):
+	ray.target_position = inputs[dir] * tile_size
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		#position += inputs[dir] * tile_size
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "position", position + inputs[dir] * tile_size, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
+		moving = true
+		animated_sprite.play("run")
+		await tween.finished
+		animated_sprite.play("default")
+		moving = false
